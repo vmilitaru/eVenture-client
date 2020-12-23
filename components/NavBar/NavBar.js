6b265b35b467db3cc2c0bfc113f5d44f4ad27fbc
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2020-12-23 17:59:45 +0000

    basic styled nav completed

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,29 +1,106 @@
-import Link from 'next/link'
-
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Link from '@material-ui/core/Link'
 const NavBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+    const classes = useStyles()
+
     return (
-        <nav>
-            <div>
-                <ul>
-                    <li>
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/events-page">
-                            <a>Event</a>
-                        </Link>
-                    </li>
-                    <li>
+        <React.Fragment>
+            <AppBar
+                position="static"
+                color="default"
+                elevation={0}
+                className={classes.appBar}
+            >
+                <Toolbar className={classes.toolbar}>
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        className={classes.toolbarTitle}
+                    >
+                        Company name
+                    </Typography>
+                    {/* <img src="../../soclogo.png" alt="logo" /> */}
+                    <Link
+                        variant="h6"
+                        color="textPrimary"
+                        href="/"
+                        className={classes.link}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        href="/events-page"
+                        variant="h6"
+                        color="textPrimary"
+                        className={classes.link}
+                    >
+                        Event
+                    </Link>
+
+                    <Button
+                        aria-controls="simple-menu"
+                        aria-haspopup="true"
+                        color="primary"
+                        variant="outlined"
+                        onClick={handleClick}
+                        className={classes.link}
+                    >
                         <Link href="/login">
-                            <a>Login</a>
+                            <Typography variant="h6">LogIn</Typography>
                         </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                    </Button>
+                    {/* <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu> */}
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
     )
 }
 
+const useStyles = makeStyles((theme) => ({
+    '@global': {
+        ul: {
+            margin: 0,
+            padding: 0,
+            listStyle: 'none'
+        }
+    },
+    appBar: {
+        borderBottom: `1px solid ${theme.palette.divider}`
+    },
+    toolbar: {
+        flexWrap: 'wrap'
+    },
+    toolbarTitle: {
+        flexGrow: 1
+    },
+    link: {
+        margin: theme.spacing(1, 10)
+    }
+}))
 export default NavBar
