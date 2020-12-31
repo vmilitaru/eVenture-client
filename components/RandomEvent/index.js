0a5464e6c3398a2ddcccfb15707f143e77c40709
michaelfswann <mikeswann@mail.com>
2020-12-31 11:18:55 +0000

    tokens working with back-end

--- components/RandomEvent/index.js
+++ components/RandomEvent/index.js
@@ -1,38 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
-const BACKEND_URL = 'http://localhost:6000'
+const BACKEND_URL = 'http://localhost:5000'
 
 const RandomEvent = () => {
-    // const { getAccessTokenSilently } = useAuth0()
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
     const [event, setEvent] = useState(null)
 
     // console.log('line8')
 
-    /*   useEffect(() => {
-        async function getEvent() {
-            const domain = 'dev-49ka9ni6.eu.auth0.com'
+    useEffect(() => {
+        if (user && isAuthenticated) {
+            async function getEvent() {
+                const domain = 'dev-49ka9ni6.eu.auth0.com'
 
-            const accessToken = await getAccessTokenSilently({
-                audience: `https://${domain}/api/v2/`,
-                scope: 'read:current_user'
-            })
+                const accessToken = await getAccessTokenSilently({
+                    audience: `https://${domain}/api/v2/`,
+                    scope: 'read:current_user'
+                })
 
-            const response = await fetch(`${BACKEND_URL}/protected/1`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            })
+                const response = await fetch(`${BACKEND_URL}/protected/1`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                })
 
-            const result = await response.json()
+                const result = await response.json()
 
-            console.log(result)
+                console.log(result)
 
-            setEvent(result)
+                setEvent(result)
+            }
+
+            getEvent()
         }
-
-        getEvent()
-    }, [event]) */
+    }, [user, isAuthenticated])
 
     return <div>hello</div>
 }
