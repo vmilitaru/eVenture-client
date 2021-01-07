55f00a79678b6a9eed47b0c7b95eac92a75fe47b
= <tomnbennett2013@gmail.com>
2021-01-07 14:59:29 +0000

    Date, time and description added to event cards

--- components/EventCard/EventCard.js
+++ components/EventCard/EventCard.js
@@ -8,8 +8,21 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import { useStyles } from './EventCardMaterialCss'
+
 export default function EventCard({ event }) {
     const classes = useStyles()
+
+    function shortenDescription() {
+        if (!event.description) {
+            return '...'
+        }
+        const descArray = event?.description.split('')
+        let shortDesc = descArray?.splice(0, 40)
+        shortDesc = shortDesc?.join('').trim()
+        shortDesc += '...'
+        return shortDesc
+    }
+
     return (
         <React.Fragment>
             <Card className={classes.root}>
@@ -23,6 +36,10 @@ export default function EventCard({ event }) {
                         <Typography gutterBottom variant="h3" component="h3">
                             {event.title}
                         </Typography>
+                        <Typography>
+                            {event.date} - {event.time}
+                        </Typography>
+                        <Typography>{shortenDescription()}</Typography>
                     </CardContent>
                 </CardActionArea>
             </Card>
