7e75bb1ecdb2c6aaaf832fbc43a1decea844a8e2
vmilitaru <vmilitaru28@gmail.com>
2021-01-07 23:49:22 +0000

    delete button added to handle the banner before submission

--- components/ImageUploader/index.js
+++ components/ImageUploader/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Card from '@material-ui/core/Card'
 import Grid from '@material-ui/core/Grid'
 import Fab from '@material-ui/core/Fab'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate'
 import { makeStyles } from '@material-ui/core/styles'
 import Image from 'next/image'
+import { flexbox } from '@material-ui/system'
 
 const useStyles = makeStyles(() => ({
     input: {
@@ -12,29 +15,41 @@ const useStyles = makeStyles(() => ({
     }
 }))
 
-const UploadImage = ({handleFileInputChange, previewSource}) => {
+const UploadImage = ({ handleFileInputChange, previewSource,setPreviewSource }) => {
     const classes = useStyles()
 
-   
-   
+    function deletePreviewBanner(){
+        setPreviewSource (null)
+    }
+
+
     return (
-        <div style={{ position: 'relative', top: '0px' }}>
-            <Card>
-                <Grid container justify="center" alignItems="center">
-                        <input
-                            onChange={handleFileInputChange}
-                            accept=".png, .jpeg, .jpg"
-                            id="contained-button-file"
-                            multiple
-                            type="file"
-                            className={classes.input}
-                        />
-                        <label htmlFor="contained-button-file">
-                            <Fab component="span">
-                                <AddPhotoAlternateIcon />
-                            </Fab>
-                        </label>
-                        {previewSource && (
+        <div>
+            {previewSource && (
+                <div>
+                    <IconButton aria-label="Delete" onClick={deletePreviewBanner}>
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
+            )}
+            {!previewSource && (
+                <div>
+                    <input
+                        onChange={handleFileInputChange}
+                        accept=".png, .jpeg, .jpg"
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                        className={classes.input}
+                    />
+                    <label htmlFor="contained-button-file">
+                        <Fab component="span">
+                            <AddPhotoAlternateIcon />
+                        </Fab>
+                    </label>
+                </div>
+            )}
+            {previewSource && (
                 <Image
                     src={previewSource}
                     alt="Event Image"
@@ -42,12 +57,15 @@ const UploadImage = ({handleFileInputChange, previewSource}) => {
                     height={400}
                 />
             )}
-                   
-                </Grid>
-            </Card>
-            
         </div>
-       
     )
 }
 export default UploadImage
+
+/* style={{
+    width: '400px',
+    height: '400px',
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center'
+}} */
